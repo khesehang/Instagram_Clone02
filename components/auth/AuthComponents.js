74f8d02b41361d7a0782e26714d89fcc8932fd85
khesehang <khesehangsamsohang004@gmail.com>
2023-07-19 01:06:55 +0545

    signin signup bcryptjs

--- components/auth/AuthComponents.js
+++ components/auth/AuthComponents.js
@@ -1,8 +1,15 @@
 const UserModel = require('../../model/user_model');
+const bcrypt = require('bcryptjs');
 
 const Register = async (req, res, next) => {
-    const newUser = new UserModel(req.body)
+    var salt = bcrypt.genSaltSync(10)
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new UserModel({
+        ...req.body,
+        password: hash,
+    })
     try {
+
         await newUser.save()
         res.json(newUser)
     } catch (error) {
@@ -10,6 +17,31 @@ const Register = async (req, res, next) => {
     }
 }
 
+const Login = async (req, res, next) => {
+    const data = req.body
+   const user = await  UserModel.findOne({
+    $or: [
+        { username: data.username },
+        { email: data.username },
+    ]
+})
+    
+        if(!user) {
+            return next({
+                msg: 'Invalid username',
+                status: 400
+            })
+        }
+
+        isMatched = await bcrypt.compare(data.password,user.password)
+        if(!isMatched) {
+            return res.json({msg: 'Username or Password not matched'})
+        }
+        
+        return res.json(user)
+}
+
 module.exports = {
-    Register
+    Register,
+    Login
 }
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,8 @@
-const { Register } = require('../components/auth/AuthComponents');
+const { Register, Login } = require('../components/auth/AuthComponents');
 
 const router = require('express').Router();
 
 router.post('/register',Register)
+router.post('/login',Login)
 
 module.exports = router;
